e7774ca402b7dac9c6014844533d048c2d66ea83
tomasekc <tomasekc@gilmour.org>
2016-11-21 10:37:36 -0500

    Cruise method speed changes - still cant find the balance

--- student.py
+++ student.py
@@ -221,7 +221,7 @@ class  GoPiggy(pigo.Pigo):
 
     #Moving slowly forever but still scanning
     def cruise(self):
-        self.setSpeed(60, 100)
+        self.setSpeed(60, 80)
         # Have the robot drive forward without end
         while self.frontClear():
             print("It is clear. Time to fly!")
