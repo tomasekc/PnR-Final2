548bcf4f8ebb4411fb92b5c7d7141725fc7f9234
tomasekc <tomasekc@gilmour.org>
2016-12-09 09:59:02 -0500

    fixing backup method

--- student.py
+++ student.py
@@ -169,7 +169,7 @@ class  GoPiggy(pigo.Pigo):
     # A SAFETY PRECAUTION
     def backUp(self):
         if us_dist(15) < 2:
-            print("I don't think it's safe. Backing up for 3/10 a second")
+            print("I don't think it's safe. Backing up for .3 second")
             bwd()
             time.sleep(.3)
             self.stop()
